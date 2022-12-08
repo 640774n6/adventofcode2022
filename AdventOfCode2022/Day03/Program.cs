003f9db7df535aca646afd7f09d8f70ed806587d
Brandon Holland <bh@bholland.dev>
2022-12-07 22:37:25 -0800

    Added day 3 and day 4 solutions

---
+++ AdventOfCode2022/Day03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2022
+{
+    public class Day03
+    {
+        public static void Main(string[] args)
+        {
+            IEnumerable<string> input = File.ReadAllText(args[0])
+                .Split("\n")
+                .Where(x => !string.IsNullOrWhiteSpace(x));
+
+            int part1 = input
+                .Select(x => x.Substring(0, (int)(x.Length / 2)).Intersect(x.Substring((int)(x.Length / 2), (int)(x.Length / 2))).First())
+                .Select(x => x >= 'A' && x <= 'Z' ? x - 38 : x - 96)
+                .Sum();
+            Console.WriteLine($"part1: {part1}");
+
+            int part2 = input
+                .Chunk(3)
+                .Select(x => x[0].Intersect(x[1]).Intersect(x[2]).First())
+                .Select(x => x >= 'A' && x <= 'Z' ? x - 38 : x - 96)
+                .Sum();
+            Console.WriteLine($"part2: {part2}");
+        }
+    }
+}
---
+++ AdventOfCode2022/Day04/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2022
+{
+    public class Day04
+    {
+        public static void Main(string[] args)
+        {
+            IEnumerable<IEnumerable<int>[]> input = File.ReadAllText(args[0])
+                .Split("\n")
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .Select(x => x.Split(",").Select(y => Enumerable.Range(int.Parse(y.Split("-")[0]), int.Parse(y.Split("-")[1]) - int.Parse(y.Split("-")[0]) + 1)).ToArray());
+
+            int part1 = input
+                .Where(x => !x[0].Except(x[1]).Any() || !x[1].Except(x[0]).Any())
+                .Count();
+            Console.WriteLine($"part1: {part1}");
+
+            int part2 = input
+                .Where(x => x[0].Intersect(x[1]).Any())
+                .Count();
+            Console.WriteLine($"part2: {part2}");
+        }
+    }
+}
